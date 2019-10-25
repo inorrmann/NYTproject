9d80eb3b25f6e9b8c76b9f3ad4d5f6bb2a98fbcb
Robert Parsons <rmpcoding@gmail.com>
2019-10-25 13:36:13 -0700

    test3 of local branch push

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,4 @@ selectParams();
 //  https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&api-key=wt46tRae2IlBCuqzk6TDVkNTV4GfblUz
 
 // testing for git push
+// testing for git push into local Robert branch
