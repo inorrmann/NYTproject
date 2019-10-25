56eb835adb9604a2beb39b2c97afbcf2a570c41e
Robert Parsons <rmpcoding@gmail.com>
2019-10-25 13:58:55 -0700

    not the latest pull from master. This is also a test. no changes in my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,8 +19,3 @@ function selectParams() {
 }
 
 selectParams();
-
-//  https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&api-key=wt46tRae2IlBCuqzk6TDVkNTV4GfblUz
-
-// testing for git push
-// testing for git push into local Robert branch
